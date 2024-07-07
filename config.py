c5e50516e9f2bdd5d19cf1488783a6ee4b652f3e
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 14:47:04 -0500

    Adding images configuration for training functions and parser train option

--- config.py
+++ config.py
@@ -1,15 +1,23 @@
 import argparse
 
-PATH_CLASS  = ["data/open_hand", "data/close_hand"]
+DATA_FOLDER = "data"
+CATEGORIES = ["open", "close"]
 CLASS_NAME = ["OPEN", "CLOSE"]
 EXTENSION = "jpg"
+MODEL_FOLDER = "models"
+IMAGE_WIDTH = 50
+IMAGE_HEIGTH = 50
+
 
 def load_parser():
     parser = argparse.ArgumentParser(description="Gesture Recongnizer desc...")
 
-    # Adding arguments
+    # Adding load data arguments
     parser.add_argument('-l', '--load', action='store_true', help="capture the hand data using the camera.")
     parser.add_argument('-o', '--open', action='store_true', help="activate open hand capture.")
     parser.add_argument('-c', '--close', action='store_true', help="activate close hand capture.")
+
+    # Adding IA arguments
+    parser.add_argument('-t', '--train', action='store_true', help="train model with loaded data.")
 
     return parser.parse_args()
