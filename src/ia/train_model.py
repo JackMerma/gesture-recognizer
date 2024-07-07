30c3fea10a3c579f5a05d98b56c218710eed1316
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 15:14:33 -0500

    Calling to load_model, training with train data, validating with test data and saving in models folder

--- src/ia/train_model.py
+++ src/ia/train_model.py
@@ -6,3 +6,23 @@ def train():
 
     # Loading data
     images, labels = load_data(DATA_FOLDER, CATEGORIES, IMAGE_WIDTH, IMAGE_HEIGTH)
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+            np.array(images), np.array(labels), test_size=TEST_SIZE
+            )
+
+    # Get a compiled model
+    model = get_model(IMAGE_WIDTH, IMAGE_HEIGTH, CATEGORIES)
+
+    # Fitting the model using training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluating model performance
+    model.evaluate(x_test, y_test, verbose=2)
+
+    # Saving model
+    model_file_path = os.path.join(MODEL_FOLDER, MODEL_NAME)
+    model.save(model_file_path)
