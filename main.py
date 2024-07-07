eb72f0960b9bdc8c54a6d49a0356de706d857e2e
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 01:24:35 -0500

    Working with load parser

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from src.capture.capture_data import *
+from config import *
 
 
 def main():
 
-    capture(0)
+    # Getting parser
+    args = load_parser()
+
+    if args.load:
+        if args.open:
+            capture(0)
+        elif args.close:
+            capture(1)
+
 
 
 if __name__ == "__main__":
