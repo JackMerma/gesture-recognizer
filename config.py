ae19d4a305ba77ccfd3935488c752a340ecfaa40
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 01:23:56 -0500

    Adding a load parser for prompt input format

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+import argparse
+
 PATH_CLASS  = ["data/open_hand", "data/close_hand"]
 CLASS_NAME = ["OPEN", "CLOSE"]
 EXTENSION = "jpg"
+
+def load_parser():
+    parser = argparse.ArgumentParser(description="Gesture Recongnizer desc...")
+
+    # Adding arguments
+    parser.add_argument('-l', '--load', action='store_true', help="capture the hand data using the camera.")
+    parser.add_argument('-o', '--open', action='store_true', help="activate open hand capture.")
+    parser.add_argument('-c', '--close', action='store_true', help="activate close hand capture.")
+
+    return parser.parse_args()
